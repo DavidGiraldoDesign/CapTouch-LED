d1cb8d188e2e43bc470356fea2fbeb7a83ddd5fe
Jose David Giraldo Mosquera <josedavidgm1995@gmail.com>
2018-10-27 10:22:17 -0500

    Added

---
+++ blink_led.py
@@ -0,0 +1,37 @@
+import RPi.GPIO as G
+import time
+
+LedPin = 4 #pin4 for the LED
+CapacitivePin = 5 #pin5 for he capacitive touch
+
+def setup():
+    G.setmode(G.BCM)
+    G.setup(CapacitivePin, G.IN)
+    G.setup(LedPin, G.OUT, initial=G.LOW)
+    
+    
+def blink():
+    while True:
+        
+        if G.input(CapacitivePin) == G.HIGH:
+            G.output(LedPin, True)
+        else:
+            G.output(LedPin, False)
+    
+        #Sketch for just blink the LED
+        #G.output(LedPin, G.HIGH)
+        #time.sleep(1)
+        #G.output(LedPin, G.LOW)
+        #time.sleep(1)
+        
+def destroy():
+    G.output(LedPin, G.LOW)
+    G.cleanup()
+    
+if __name__ == '__main__':
+    setup()
+    try:
+        blink()
+    except KeyboardInterrupt:
+        destroy()
+        
